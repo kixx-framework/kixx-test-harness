458513c2ee431e5b708a975d7ae6339216ea54ce
Kris Walker <kris@kixx.name>
2026-06-29 16:02:15 -0400

    Start admin bootstrap test
     Changes to be committed:
    	modified:   tests/001-bootstrap-new-deployment.test.js

--- tests/001-bootstrap-new-deployment.test.js
+++ tests/001-bootstrap-new-deployment.test.js
@@ -30,3 +30,44 @@ describe('check the bootstrap token', ({ it }) => {
         assertEqual('placeholder-bootstrap-token', process.env.ADMIN_BOOTSTRAP_TOKEN);
     });
 });
+
+describe('accept bootstrap invite with invalid token', ({ before, it }) => {
+    const baseURL = process.env.TEST_SERVER_BASE_URL;
+
+    const url = new URL(`${ baseURL }/admin-api/v1/users/invite`);
+
+    const data = {
+        type: 'AdminUser',
+        attributes: {
+            emailAddress: 'Margaret.Hamilton@example.com',
+            password: 'Apollo11',
+        },
+    };
+
+    let response;
+    let body;
+
+    before(async () => {
+        const payload = JSON.stringify({ data });
+        const contentLength = new Blob([ payload ]).size;
+
+        response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                authorization: 'Bearer some-invalid-token',
+                'content-type': 'application/vnd.api+json; charset=utf-8',
+                'content-length': contentLength,
+            },
+            body: payload,
+        });
+
+        body = await response.json();
+    });
+
+    it('failed', () => {
+        assert(response);
+        assertEqual(404, response.status);
+        assertEqual('application/json; charset=utf-8', response.headers.get('content-type'));
+        assertArray(body.errors);
+    });
+});
