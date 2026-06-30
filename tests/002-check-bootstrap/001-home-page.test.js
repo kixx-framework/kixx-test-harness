97ea0142d224a5862859475be6845475644158af
Kris Walker <kris@kixx.name>
2026-06-30 13:05:01 -0400

    Test home page bootstrap content
     Changes to be committed:
    modified:   tests/002-check-bootstrap/001-home-page.test.js

--- tests/002-check-bootstrap/001-home-page.test.js
+++ tests/002-check-bootstrap/001-home-page.test.js
@@ -48,4 +48,44 @@ describe('check home page html', ({ before, it }) => {
         assertEqual('http://localhost:2026/', document.getElementById('link-canonical').getAttribute('href'));
         assertEqual('/stylesheets/0.1.0/website.css', document.getElementById('link-stylesheet').getAttribute('href'));
     });
+
+    it('has expected content', () => {
+        const document = new FastHTMLParser(body);
+        const header = document.getElementById('branded-page-header');
+        assertEqual('This is the branded page header', header.getElementsByTagName('p')[0].textContent);
+        const [ main ] = document.getElementsByTagName('main');
+        assertEqual('Home Page', main.getElementsByTagName('h1')[0].textContent);
+    });
+});
+
+describe('check home page JSON', ({ before, it }) => {
+    let response;
+    let body;
+
+    before(async () => {
+        const url = new URL(`${ baseURL }/index.json`);
+        response = await fetch(url);
+        body = await response.json();
+    });
+
+    it('responds with an HTTP 200 status', () => {
+        assertEqual(200, response.status);
+    });
+
+    it('responds with JSON', () => {
+        assertMatches('application/json', response.headers.get('content-type'));
+        assert(body);
+    });
+
+    it('has expected metadata', () => {
+        const {
+            build_id,
+            version,
+            baseTemplate,
+        } = body;
+
+        assertEqual('0.1.0', build_id);
+        assertEqual('v0-bootstrap', version);
+        assertEqual('base.html', baseTemplate);
+    });
 });
