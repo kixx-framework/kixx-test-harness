9197c613874d087387cb8e7fe5680480e714cc3a
Kris Walker <kris@kixx.name>
2026-06-30 12:56:45 -0400

    Checking the bootstrapped home page
     Changes to be committed:
    	modified:   fixtures/v0-bootstap/templates/base/base.html
    	modified:   package.json
    	new file:   tests/002-check-bootstrap/001-home-page.test.js

---
+++ tests/002-check-bootstrap/001-home-page.test.js
@@ -0,0 +1,51 @@
+import process from 'node:process';
+import { describe } from 'kixx-test';
+import { FastHTMLParser } from 'fast-html-dom-parser';
+import {
+    assert,
+    assertEqual,
+    assertMatches,
+} from 'kixx-assert';
+
+// import { BUILD_IDS } from '../../lib/constants.js';
+
+const baseURL = process.env.TEST_SERVER_BASE_URL;
+
+
+describe('check home page html', ({ before, it }) => {
+    let response;
+    let body;
+
+    before(async () => {
+        response = await fetch(baseURL);
+        body = await response.text();
+    });
+
+    it('responds with an HTTP 200 status', () => {
+        assertEqual(200, response.status);
+    });
+
+    it('responds with HTML', () => {
+        assertMatches('text/html', response.headers.get('content-type'));
+        assert(body);
+    });
+
+    it('has expected metadata', () => {
+        const document = new FastHTMLParser(body);
+        const [ root ] = document.getElementsByTagName('html');
+        assertEqual('en-US', root.getAttribute('lang'));
+        const [ head ] = root.getElementsByTagName('head');
+        const [ title ] = head.getElementsByTagName('title');
+        const [ description ] = head.getElementsByName('description');
+        assertEqual('Blank Website', title.textContent);
+        assertEqual('Blank website scaffolding', description.getAttribute('content'));
+        assertEqual('website', document.getElementById('og-type').getAttribute('content'));
+        assertEqual('Blank Website', document.getElementById('og-title').getAttribute('content'));
+        assertEqual('Blank website scaffolding', document.getElementById('og-description').getAttribute('content'));
+        assertEqual('http://localhost:2026/', document.getElementById('og-url').getAttribute('content'));
+        assertEqual('en-US', document.getElementById('og-locale').getAttribute('content'));
+        assertEqual('', document.getElementById('og-image').getAttribute('content'));
+        assertEqual('http://localhost:2026/', document.getElementById('link-canonical').getAttribute('href'));
+        assertEqual('/stylesheets/0.1.0/website.css', document.getElementById('link-stylesheet').getAttribute('href'));
+    });
+});
