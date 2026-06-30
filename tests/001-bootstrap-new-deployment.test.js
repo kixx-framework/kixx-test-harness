7c76b171ee0a6d5dcb9df61932c661a0b26c9bf0
Kris Walker <kris@kixx.name>
2026-06-30 04:40:11 -0400

    Test API accept invite
     Changes to be committed:
    	modified:   tests/001-bootstrap-new-deployment.test.js

--- tests/001-bootstrap-new-deployment.test.js
+++ tests/001-bootstrap-new-deployment.test.js
@@ -1,6 +1,18 @@
 import process from 'node:process';
 import { describe } from 'kixx-test';
-import { assert, assertEqual, assertArray } from 'kixx-assert';
+import {
+    assert,
+    assertEqual,
+    assertArray,
+    assertMatches,
+    assertNonEmptyString,
+} from 'kixx-assert';
+
+
+const ADMIN_USER = {
+    emailAddress: 'Margaret.Hamilton@example.com',
+    password: 'Apollo11-to-the-M00N',
+};
 
 
 describe('smoke check the server', ({ before, it }) => {
@@ -27,7 +39,7 @@ describe('smoke check the server', ({ before, it }) => {
 
 describe('check the bootstrap token', ({ it }) => {
     it('has set the ADMIN_BOOTSTRAP_TOKEN env var', () => {
-        assertEqual('placeholder-bootstrap-token', process.env.ADMIN_BOOTSTRAP_TOKEN);
+        assertNonEmptyString(process.env.ADMIN_BOOTSTRAP_TOKEN);
     });
 });
 
@@ -39,8 +51,8 @@ describe('accept bootstrap invite with invalid token', ({ before, it }) => {
     const data = {
         type: 'AdminUser',
         attributes: {
-            emailAddress: 'Margaret.Hamilton@example.com',
-            password: 'Apollo11',
+            emailAddress: ADMIN_USER.emailAddress,
+            password: ADMIN_USER.password,
         },
     };
 
@@ -64,10 +76,150 @@ describe('accept bootstrap invite with invalid token', ({ before, it }) => {
         body = await response.json();
     });
 
-    it('failed', () => {
+    it('is forbidden', () => {
         assert(response);
-        assertEqual(404, response.status);
-        assertEqual('application/json; charset=utf-8', response.headers.get('content-type'));
+        assertEqual(403, response.status);
+        assertEqual('application/vnd.api+json; charset=utf-8', response.headers.get('content-type'));
         assertArray(body.errors);
+        const error = body.errors[0];
+        assertEqual('403', error.status);
+        assertEqual('InvalidInvite', error.code);
+    });
+});
+
+describe('accept bootstrap invite with invalid password', ({ before, it }) => {
+    const baseURL = process.env.TEST_SERVER_BASE_URL;
+    const bootstrapToken = process.env.ADMIN_BOOTSTRAP_TOKEN;
+
+    const url = new URL(`${ baseURL }/admin-api/v1/users/invite`);
+
+    const data = {
+        type: 'AdminUser',
+        attributes: {
+            emailAddress: ADMIN_USER.emailAddress,
+            password: 'too_short',
+        },
+    };
+
+    let response;
+    let body;
+
+    before(async () => {
+        const payload = JSON.stringify({ data });
+        const contentLength = new Blob([ payload ]).size;
+
+        response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${ bootstrapToken }`,
+                'content-type': 'application/vnd.api+json; charset=utf-8',
+                'content-length': contentLength,
+            },
+            body: payload,
+        });
+
+        body = await response.json();
+    });
+
+    it('failed with validation error', () => {
+        assert(response);
+        assertEqual(422, response.status);
+        assertEqual('application/vnd.api+json; charset=utf-8', response.headers.get('content-type'));
+        assertArray(body.errors);
+        const error = body.errors[0];
+        assertEqual('422', error.status);
+        assertEqual('VALIDATION_ERROR', error.code);
+        assertMatches('password', error.source);
+    });
+});
+
+describe('accept bootstrap invite', ({ before, it }) => {
+    const baseURL = process.env.TEST_SERVER_BASE_URL;
+    const bootstrapToken = process.env.ADMIN_BOOTSTRAP_TOKEN;
+
+    const url = new URL(`${ baseURL }/admin-api/v1/users/invite`);
+
+    const data = {
+        type: 'AdminUser',
+        attributes: {
+            emailAddress: ADMIN_USER.emailAddress,
+            password: ADMIN_USER.password,
+        },
+    };
+
+    let response;
+    let body;
+
+    before(async () => {
+        const payload = JSON.stringify({ data });
+        const contentLength = new Blob([ payload ]).size;
+
+        response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${ bootstrapToken }`,
+                'content-type': 'application/vnd.api+json; charset=utf-8',
+                'content-length': contentLength,
+            },
+            body: payload,
+        });
+
+        body = await response.json();
+    });
+
+    it('succeeded', () => {
+        assert(response);
+        assertEqual(201, response.status);
+        assertEqual('application/vnd.api+json; charset=utf-8', response.headers.get('content-type'));
+        assert(body.data);
+        const { data } = body;
+        assertEqual('AdminUser', data.type);
+        assertNonEmptyString(data.id);
+        assertEqual(ADMIN_USER.emailAddress.toLowerCase(), data.attributes.emailAddress);
+    });
+});
+
+describe('accept bootstrap invite when user exists', ({ before, it }) => {
+    const baseURL = process.env.TEST_SERVER_BASE_URL;
+    const bootstrapToken = process.env.ADMIN_BOOTSTRAP_TOKEN;
+
+    const url = new URL(`${ baseURL }/admin-api/v1/users/invite`);
+
+    const data = {
+        type: 'AdminUser',
+        attributes: {
+            emailAddress: 'bad-actor@gmail.com',
+            password: 'a good enough password',
+        },
+    };
+
+    let response;
+    let body;
+
+    before(async () => {
+        const payload = JSON.stringify({ data });
+        const contentLength = new Blob([ payload ]).size;
+
+        response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${ bootstrapToken }`,
+                'content-type': 'application/vnd.api+json; charset=utf-8',
+                'content-length': contentLength,
+            },
+            body: payload,
+        });
+
+        body = await response.json();
+    });
+
+    it('is forbidden', () => {
+        assert(response);
+        assertEqual(403, response.status);
+        assertEqual('application/vnd.api+json; charset=utf-8', response.headers.get('content-type'));
+        assertArray(body.errors);
+        const error = body.errors[0];
+        assertEqual('403', error.status);
+        assertEqual('InvalidInvite', error.code);
     });
 });
